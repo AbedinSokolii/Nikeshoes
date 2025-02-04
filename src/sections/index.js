219d54aa3f2579b8cbb93d989a89402678663771
Abedin Sokoli <129113940+AbedinSokolii@users.noreply.github.com>
2025-02-04 22:54:59 +0100

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustumerReviews from "./CustumerReviews";
+import Subscribe from "./Subscribe";
+
+
+export {
+    Hero,
+    PopularProducts,
+    Footer,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustumerReviews,
+    Subscribe
+}
